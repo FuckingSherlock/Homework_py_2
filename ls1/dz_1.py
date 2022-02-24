62a55fb3fba5f61000fc8e9b33d8bc2670f9a40c
Kirill <FuckingSherlock@users.noreply.github.com>
2022-02-25 02:27:24 +0300

    del

--- ls1/dz_1.py
+++
@@ -1,9 +0,0 @@
-# 1. Найти сумму и произведение цифр трехзначного числа, которое вводит пользователь.
-num = int(input())
-first = num // 100
-second = num // 10 % 10
-third = num % 10
-mult = first*second*third
-print('Произведение цифр числа =', mult)
-amount = first+second+third
-print('Сумма цифр числа =', amount)
